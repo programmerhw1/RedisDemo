44c409f4d0d871a249b47d5dff658a20c8ffb5cc
kaka <1823188198@qq.com>
2026-05-23 17:51:01 +0800

    feat:修复功能

--- src/main/java/com/yuanaan/redisdemo/demos/web/User.java
+++ src/main/java/com/yuanaan/redisdemo/demos/web/User.java
@@ -40,4 +40,8 @@ public class User {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    public static void main(String[] args) {
+        System.out.println("垃圾桶持续产出");
+    }
 }
